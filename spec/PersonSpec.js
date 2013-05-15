7a9d0d44cf1b334992e88dc7e1a7c02d935371f2
RyanDurling <ryan.p.durling@gmail.com>
2013-05-15 07:46:15 -0700

    add should be editable to test

--- spec/PersonSpec.js
+++ spec/PersonSpec.js
@@ -1,9 +1,36 @@
 describe("Person", function(){
 
+    var johnDoe;
+
+    beforeEach(function() {
+	johnDoe = new Person("John", "Doe", 40);
+    });
+
+    it("should be defined when created", function() {
+        expect(johnDoe).toBeDefined();
+    });
+
     it("Should have the correct first name", function() {
+        expect(johnDoe.firstName).toBe("John");
+    });
 
-	var johnDoe = new Person("John", "Doe", 40);
+    it("should have the correct age", function() {
+	expect(johnDoe.age).toBe(40);
+    });
 
-	expect(johnDoe.firstName).toBe("John");
+    it("should have the correct last name", function() {
+	expect(johnDoe.lastName).toBe("Doe");
+    });
+
+    it("should be editable", function() {
+	johnDoe.age = 34;
+	expect(johnDoe.age).not.toBe(40);
+	expect(johnDoe.age).toBe(34);
+	johnDoe.firstName = "Ryan";
+	expect(johnDoe.firstName).not.toBe("John");
+	expect(johnDoe.firstName).toBe("Ryan");
+	johnDoe.lastName = "Durling";
+	expect(johnDoe.lastName).not.toBe("Doe");
+	expect(johnDoe.lastName).toBe("Durling");
     });
 });
